3d77d4cd31ee554cfb6c68d03276b8940236c745
dangkhoabach <dangkhoabach2002@gmail.com>
2023-04-18 10:10:04 +0700

    Update 18/04/2023 10h

---
+++ CinemaTicketHub/Controllers/BookingController.cs
@@ -0,0 +1,25 @@
+﻿using CinemaTicketHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CinemaTicketHub.Controllers
+{
+    public class BookingController : Controller
+    {
+        ApplicationDbContext _dbContext = new ApplicationDbContext();
+
+        // GET: Booking
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SeatSelection()
+        {
+            return View();
+        }
+    }
+}
