e494dc9ce1bebd88d48c975891b9dd734927867a
dangkhoabach <dangkhoabach2002@gmail.com>
2023-05-14 22:00:51 +0700

    Update 14/05/2023 22h

--- CinemaTicketHub/Controllers/BookingController.cs
+++ CinemaTicketHub/Controllers/BookingController.cs
@@ -1,4 +1,5 @@
-﻿using CinemaTicketHub.Models;
+﻿using Antlr.Runtime.Tree;
+using CinemaTicketHub.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,15 +20,63 @@ namespace CinemaTicketHub.Controllers
 
         public ActionResult SeatSelection(int MaSuatChieu)
         {
-            /*var seat = _dbContext.Ghe.Where(p => p.MaSuatChieu == MaSuatChieu).OrderBy(o => o.Day).ThenBy(t => t.Cot).ToList();*/
-            ViewBag.rowA = _dbContext.Ghe.Where(x => x.MaSuatChieu == MaSuatChieu && x.Day == "A").ToList();
-            ViewBag.rowB = _dbContext.Ghe.Where(x => x.MaSuatChieu == MaSuatChieu && x.Day == "B").ToList();
-            ViewBag.rowC = _dbContext.Ghe.Where(x => x.MaSuatChieu == MaSuatChieu && x.Day == "C").ToList();
-            ViewBag.rowD = _dbContext.Ghe.Where(x => x.MaSuatChieu == MaSuatChieu && x.Day == "D").ToList();
-            ViewBag.rowE = _dbContext.Ghe.Where(x => x.MaSuatChieu == MaSuatChieu && x.Day == "E").ToList();
+            ViewBag.moviesdetail = _dbContext.SuatChieu.Where(y => y.MaSuatChieu == MaSuatChieu).FirstOrDefault();
+            SelectSeatDataModel selectSeatDataModel = new SelectSeatDataModel();
+            selectSeatDataModel.listcheck = lstCheckList(MaSuatChieu);
 
-            var moviesdetail = _dbContext.SuatChieu.Where(y => y.MaSuatChieu == MaSuatChieu).FirstOrDefault();
-            return View(moviesdetail);
+            return View(selectSeatDataModel);
+        }
+
+        [HttpPost]
+        public ActionResult SeatSelection(SelectSeatDataModel selectSeatDataModel, int MaSC)
+        {
+            foreach (var item in selectSeatDataModel.listcheck)
+            {
+                if (item.Selected == true)
+                {
+                    var seatselected = _dbContext.Ghe.Where(p => p.MaSuatChieu == item.MaSuatChieu && p.MaGhe == item.MaGhe).FirstOrDefault();
+
+                    List<Ghe> lstGhe = Session["Cart"] as List<Ghe>;
+                    if (lstGhe == null)
+                    {
+                        lstGhe = new List<Ghe>();
+                        Session["Cart"] = lstGhe;
+                    }
+
+                    lstGhe.Add(seatselected);
+                }
+            }
+            return RedirectToAction("Cart", "Booking", new { masc = MaSC});
+        }
+
+        public List<CheckListModel> lstCheckList(int MaSuatChieu)
+        {
+
+            var seat = _dbContext.Ghe.Where(p => p.MaSuatChieu == MaSuatChieu).OrderBy(o => o.Day).ThenBy(t => t.Cot).ToList();
+
+            List<CheckListModel> lst = seat.Select((s, index) => new CheckListModel
+            {
+                SeatId = index + 1,
+                Name = s.Day + s.Cot,
+                Status = s.TrangThai,
+                MaGhe = s.MaGhe,
+                MaSuatChieu = s.MaSuatChieu
+            }).ToList();
+
+            return lst;
+        }
+
+        public ActionResult Cart(int masc)
+        {
+            List<Ghe> lstGhe = Session["Cart"] as List<Ghe>;
+            if (lstGhe == null)
+            {
+                lstGhe = new List<Ghe>();
+                Session["Cart"] = lstGhe;
+            }
+
+            ViewBag.moviesdetail = _dbContext.SuatChieu.Where(y => y.MaSuatChieu == masc).FirstOrDefault();
+            return View(lstGhe);
         }
     }
 }
---
+++ CinemaTicketHub/Models/SelectSeatDataModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CinemaTicketHub.Models
+{
+    public class SelectSeatDataModel
+    {
+        public int SeatId { get; set; }
+        public string Name { get; set; }
+        public bool? Status { get; set; }
+        public bool Selected { get; set; }
+        public int MaGhe { get; set; }
+        public int? MaSuatChieu { get; set; }
+
+        public List<CheckListModel> listcheck { get; set; }
+    }
+
+    public class CheckListModel
+    {
+        public int SeatId { get; set; }
+        public string Name { get; set; }
+        public bool? Status { get; set; }
+        public bool Selected { get; set; }
+        public int MaGhe { get; set; }
+        public int? MaSuatChieu { get; set; }
+    }
+}
