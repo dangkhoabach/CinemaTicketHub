412cf231cfec9b2013d38f1f4d7d66ec7eb5a937
dangkhoabach <dangkhoabach2002@gmail.com>
2023-11-19 20:20:44 +0700

    Update 19/11/2023 20h20 (Swagger + API)

---
+++ CinemaTicketHub/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace CinemaTicketHub.App_Start
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Đăng ký các định dạng truyền tải dữ liệu (JSON, XML, v.v.)
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+
+            // Cấu hình định tuyến cho Web API
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ CinemaTicketHub/Areas/Admin/Controllers/ShowtimesAPIController.cs
@@ -0,0 +1,75 @@
+﻿using CinemaTicketHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CinemaTicketHub.Areas.Admin.Controllers
+{
+    public class ShowtimesAPIController : ApiController
+    {
+        /*public static List<Test> testlist = new List<Test>
+        {
+            new Test {Id = 1, Title = "ABC", Description = "áddasdassad"},
+            new Test {Id = 2, Title = "XYZ", Description = "dsfsdfdsfsdf"},
+            new Test {Id = 3, Title = "WAD", Description = "tghfhfghgfh"},
+            new Test {Id = 4, Title = "QWE", Description = "yutyuytutyu"},
+            new Test {Id = 5, Title = "JKL", Description = "bnvbnvbnvbn"}
+        };
+
+        public List<Test> Get()
+        {
+            return testlist;
+        }
+
+        public Test Get(int Id)
+        {
+            return testlist.FirstOrDefault(e => e.Id == Id);
+        }*/
+
+
+        private readonly ApplicationDbContext _dbContext;
+
+        public ShowtimesAPIController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        // Phương thức GET để lấy dữ liệu từ bảng SuatChieu
+        public IHttpActionResult GetSuatChieuData()
+        {
+            try
+            {
+                // Lấy danh sách các SuatChieu từ DbContext
+                List<SuatChieu> suatChieuList = _dbContext.SuatChieu.ToList();
+
+                if (suatChieuList.Count == 0)
+                {
+                    // Trả về 204 No Content nếu không có dữ liệu
+                    return StatusCode(System.Net.HttpStatusCode.NoContent);
+                }
+
+                // Chỉ lấy các trường cần thiết và tạo danh sách DTO
+                var suatChieuDTOList = suatChieuList.Select(sc => new
+                {
+                    sc.MaSuatChieu,
+                    sc.GioBatDau,
+                    sc.GioKetThuc,
+                    sc.NgayChieu,
+                    sc.MaPhim,
+                    sc.MaPhong
+                }).ToList();
+
+                // Trả về danh sách SuatChieuDTO
+                return Ok(suatChieuDTOList);
+            }
+            catch (Exception ex)
+            {
+                // Xử lý các ngoại lệ nếu có
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
